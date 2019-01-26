b21ba84fc97cbb8d83b5b1c519066d8c5f1d6680
Stefano <nano-971@hotmail.com>
2019-01-26 04:00:38 +0100

    Adda basic player for moving in scene

---
+++ Global Game Jam/Assets/Script/CustomCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f, runSpeed = 8.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Running action
+        float actualSpeed = Input.GetButton("Jump") ? runSpeed : speed;
+
+        float zMove = Input.GetAxis("Vertical") * Time.deltaTime * actualSpeed;
+        float xMove = Input.GetAxis("Horizontal") * Time.deltaTime * actualSpeed;
+
+        transform.Translate(xMove, 0, zMove);
+    }
+}
