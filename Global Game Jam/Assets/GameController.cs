7e3c9dd4b3ce7ad0be373492240dbd1daa48616d
Gero Müller <post@geromueller.de>
2019-01-26 01:23:39 +0100

    day/night and ground

---
+++ Global Game Jam/Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private float timeOfDay;
+
+
+    // how fast time of day changes
+    public float timeFactor = 1;
+
+    public Light lights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOfDay = (timeFactor * Time.time) % 24;
+
+        lights.intensity = Mathf.InverseLerp(0, 24, timeOfDay);
+    }
+}
