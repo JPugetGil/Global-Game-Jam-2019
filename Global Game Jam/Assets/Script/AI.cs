9612cba419cd24971e9f638d08e12b423c7596c6
GIL JEROME <jerome.gil@etu.univ-lyon1.fr>
2019-01-26 16:48:32 +0100

    AI begins
    Début de la recherche du personnage et de la traque

--- Global Game Jam/Assets/Script/AI.cs
+++ Global Game Jam/Assets/Script/AI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
@@ -9,9 +10,15 @@ public class AI : MonoBehaviour
     private int bonusIA;
     private double distanceWithPlayer;
     private double probability;
-    private bool seeThePlayer;
-    private bool attack;
 
+    [SerializeField]
+    private float rotationSpeed = 1;
+
+    [SerializeField]
+    private GameObject gameControllerObject;
+
+    private NavMeshAgent agent;
+    private GameController gameController;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,46 +26,57 @@ public class AI : MonoBehaviour
         probability = 0;
         speed = 1;
         bonusIA = 0;
-        seeThePlayer = false;
-        attack = false;
+        gameController = gameControllerObject.GetComponent<GameController>();
+        agent = GetComponent<NavMeshAgent>();
 
         print("Je suis né pour te hanter...");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 ghostPos = GameObject.FindGameObjectWithTag("ghost").transform.position;
         Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
 
-        Ray ray = new Ray(ghostPos, playerPos);
+        Ray ray = new Ray(ghostPos, transform.forward);
+        Debug.DrawRay(ghostPos, transform.forward, Color.yellow);
+
         // UPDATE THE DISTANCE
         distanceWithPlayer = Vector3.Distance(ghostPos, playerPos);
 
-        //UPDATE THE PROBABILITY
-        updateProbability();
+        if (!gameController.getIsHidden() && canSeeThePlayer(ray))
+        {
+            //UPDATE THE PROBABILITY
+            probability = updateProbability();
 
-        //UPDATE IF IT CAN SEE PLAYER
+            if (Random.Range(0, 1) <= probability)
+            {
+                Debug.Log("Je vais vers toi !");
+                agent.destination = playerPos;
+            }
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0));
 
-        seeThePlayer = canSeeThePlayer(ray);
-
-        //UPDATE IF IT GOES ON THE PLAYER
-        attack = goesOnPlayer();
-
+        }
     }
 
     double updateProbability()
     {
-        if (distanceWithPlayer < 2)
+        if (distanceWithPlayer < 3)
         {
+            Debug.Log("Proba: 1");
             return 1.0;
         }
         else if (distanceWithPlayer < 3.67)
         {
+            Debug.Log("Proba: " + probability);
             return (distanceWithPlayer / (-10)) + 1.2;
         }
         else
         {
+            Debug.Log("Proba: " + probability);
             return (distanceWithPlayer / (-4)) + 1.75;
         }
     }
@@ -68,16 +86,13 @@ public class AI : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 7.0F))
         {
+            Debug.Log("Je te vois !");
             return (hit.transform.CompareTag("Player"));
         }
-
+        Debug.Log("Je ne te vois pas !");
         return false;
     }
 
-    bool goesOnPlayer()
-    {
-        return (Random.Range(0, (float)probability) < probability);
-    }
 
     //EVENTS LISTENERS FOR DAYS AND NIGHTS
 } 
